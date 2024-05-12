2e40ca506ac642f9e9c92596ca546e8f72ccd111
ifshad <shoronshark@gmail.com>
2024-05-12 13:52:44 +0600

    server updated

--- server/index.js
+++ server/index.js
@@ -6,9 +6,16 @@ const cors = require("cors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = 3000;
 
-app.use(cors({
-    origin: "http://localhost:5500" // restrict calls to those this address
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5500", // restrict calls to those this address
+  })
+);
+
+const storeItems = new Map([
+  [1, { priceInCents: 10000, name: "Learn React Today" }],
+  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+]);
 
 app.get("/", (req, res) => {
   res.send("This is Server. Lets go...");
@@ -17,6 +24,33 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   res.send("Got a POST request");
 });
+
+app.post("/create-checkout-session", async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: req.body.items.map((item) => {
+            const storeItem = storeItems.get(item.id);
+            return {
+            price_data: {
+                currency: "usd",
+                product_data: {
+                name: storeItem.name,
+                },
+                unit_amount: storeItem.priceInCents,
+            },
+            quantity: item.quantity,
+            };
+        }),
+        success_url: `${process.env.CLIENT_URL}/success.html`,
+        cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+        });
+        res.json({ url: session.url });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+    });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
